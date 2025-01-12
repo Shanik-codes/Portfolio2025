ce24cc1f8e5220e06a97c556c3918b967b9c3f0d
ShanikH <61213056+Shanik-codes@users.noreply.github.com>
2025-01-12 17:21:12 -0500

    Create HomeSection.vue

---
+++ my-portfolio/src/views/HomeSection.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+      <h1>Home</h1>
+      <p>Welcome to my portfolio!</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'HomeSection'
+  }
+  </script>
