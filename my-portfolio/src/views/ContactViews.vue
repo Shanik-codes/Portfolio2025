087be3b29c59419403bf709a5b2cdecaa01aa0cf
ShanikH <61213056+Shanik-codes@users.noreply.github.com>
2025-01-12 17:33:52 -0500

    Create ContactViews.vue

---
+++ my-portfolio/src/views/ContactViews.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <ContactSection />
+    </div>
+  </template>
+  
+  <script>
+  import ContactSection from '../components/ContactSection.vue';
+  
+  export default {
+    name: 'Contact',
+    components: {
+      ContactSection
+    }
+  }
+  </script>
