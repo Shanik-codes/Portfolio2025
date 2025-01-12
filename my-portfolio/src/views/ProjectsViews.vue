2c4204f5f41c043c48cacab13cdfc9d483182a1e
ShanikH <61213056+Shanik-codes@users.noreply.github.com>
2025-01-12 17:32:58 -0500

    Update ProjectsViews.vue

---
+++ my-portfolio/src/views/ProjectsViews.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <ProjectsSection />
+    </div>
+  </template>
+  
+  <script>
+  import ProjectsSection from '../components/ProjectsSection.vue';
+  
+  export default {
+    name: 'Projects',
+    components: {
+      ProjectsSection
+    }
+  }
+  </script>
