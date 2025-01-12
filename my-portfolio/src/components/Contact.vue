fe8e17206f53c6bbe784c18f157ad9ded996a976
ShanikH <61213056+Shanik-codes@users.noreply.github.com>
2025-01-12 16:18:51 -0500

    Update Contact.vue

---
+++ my-portfolio/src/components/Contact.vue
@@ -0,0 +1,29 @@
+<section id="contact">
+    <h2>Contact</h2>
+    <form @submit.prevent="handleSubmit">
+      <label for="name">Name:</label>
+      <input type="text" id="name" v-model="formData.name">
+      <label for="email">Email:</label>
+      <input type="email" id="email" v-model="formData.email">
+      <button type="submit">Submit</button>
+    </form>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      formData: {
+        name: '',
+        email: ''
+      }
+    };
+  },
+  methods: {
+    handleSubmit() {
+      console.log('Form submitted:', this.formData);
+    }
+  }
+}
+</script>
