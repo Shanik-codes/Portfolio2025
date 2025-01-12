4285c757e6203dda58edf8622cf3372c4a511f42
ShanikH <61213056+Shanik-codes@users.noreply.github.com>
2025-01-12 16:47:49 -0500

    Update Projects.vue

---
+++ my-portfolio/src/components/Projects.vue
@@ -0,0 +1,19 @@
+<template>
+    <section id="projects">
+      <h2>Projects</h2>
+      <div class="project">
+        <h3>Project Title</h3>
+        <p>Project description.</p>
+      </div>
+    </section>
+  </template>
+  
+  <script>
+  export default {
+    name: 'ProjectsSection'
+  }
+  </script>
+  
+  <style scoped>
+  /* Add your styles here */
+  </style>
