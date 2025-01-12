97bd31a422033d9888f425d24971930770915699
ShanikH <61213056+Shanik-codes@users.noreply.github.com>
2025-01-12 17:31:46 -0500

    Update AboutViews.vue

---
+++ my-portfolio/src/views/AboutViews.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <AboutSection />
+    </div>
+  </template>
+  
+  <script>
+  import AboutSection from '../components/AboutSection.vue';
+  
+  export default {
+    name: 'About',
+    components: {
+      AboutSection
+    }
+  }
+  </script>
