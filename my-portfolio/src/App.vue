234b54bd4cfda77c1c93f47cf6dc4c8137331af8
ShanikH <61213056+Shanik-codes@users.noreply.github.com>
2025-01-12 17:22:00 -0500

    Update App.vue

--- my-portfolio/src/App.vue
+++ my-portfolio/src/App.vue
@@ -1,25 +1,17 @@
 <template>
   <div id="app">
     <HeaderSection />
-    <AboutSection />
-    <ProjectsSection />
-    <ContactSection />
+    <router-view />
   </div>
 </template>
 
 <script>
 import HeaderSection from './components/HeaderSection.vue';
-import AboutSection from './components/AboutSection.vue';
-import ProjectsSection from './components/ProjectsSection.vue';
-import ContactSection from './components/ContactSection.vue';
 
 export default {
   name: 'App',
   components: {
-    HeaderSection,
-    AboutSection,
-    ProjectsSection,
-    ContactSection
+    HeaderSection
   }
 }
 </script>
