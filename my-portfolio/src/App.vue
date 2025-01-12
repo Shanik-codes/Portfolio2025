bae69c636212933fb55b197d4e1d16f87ec1e424
ShanikH <61213056+Shanik-codes@users.noreply.github.com>
2025-01-12 16:37:15 -0500

    Update App.vue

--- my-portfolio/src/App.vue
+++ my-portfolio/src/App.vue
@@ -1,26 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <Header />
+    <About />
+    <Projects />
+    <Contact />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue';
+import About from './components/About.vue';
+import Projects from './components/Projects.vue';
+import Contact from './components/Contact.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    About,
+    Projects,
+    Contact
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/* Add your global styles here */
 </style>
