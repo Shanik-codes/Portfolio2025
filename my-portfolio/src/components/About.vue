e47302539b91a80f9eea6f76b60cacc67a5f1262
ShanikH <61213056+Shanik-codes@users.noreply.github.com>
2025-01-12 16:47:27 -0500

    Update About.vue

---
+++ my-portfolio/src/components/About.vue
@@ -0,0 +1,16 @@
+<template>
+    <section id="about">
+      <h2>About Me</h2>
+      <p>Write a brief introduction about yourself.</p>
+    </section>
+  </template>
+  
+  <script>
+  export default {
+    name: 'AboutSection'
+  }
+  </script>
+  
+  <style scoped>
+  /* Add your styles here */
+  </style>
